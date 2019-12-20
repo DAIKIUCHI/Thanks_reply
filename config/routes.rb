5584843e3260be69d1932bf2e4185d4b2d3c82f0
Daiki Uchiyama <sijimi777asari@gmail.com>
2019-12-20 22:44:26 +0900

    staticpages_controllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  # root 'home#index'
+  get 'static_pages/home'
+  get 'static_pages/faq'
+  root 'home#index'
   get 'home/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
