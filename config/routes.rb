8a6c7054b40c5ab66bb3658d259766d5934babcd
Daiki Uchiyama <sijimi777asari@gmail.com>
2019-12-21 12:31:46 +0900

    rootページを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/faq'
-  root 'home#index'
-  get 'home/index'
+  # root 'home#index'
+  # get 'home/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
 end
