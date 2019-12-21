b1732d6b8a64703eb9e9a4c0bb2a2753abe29430
Daiki Uchiyama <sijimi777asari@gmail.com>
2019-12-21 13:59:47 +0900

    ログアウト機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # ログアウト
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/faq'
-  # root 'home#index'
-  # get 'home/index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
 end
