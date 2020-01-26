29627bd667083c8b1d482b03758999fae79d4481
Daiki Uchiyama <sijimi777asari@gmail.com>
2020-01-26 19:52:54 +0900

    ログイン時のflash追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_oauth(request.env['omniauth.auth'])
  
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      # flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      flash[:success] = "ログインしました"
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
